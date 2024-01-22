8e269e4a12f67c57ecccce812f6a7aa10a247ad5
Liam Dyer <liamcdyer@gmail.com>
2024-01-22 18:11:57 -0500

    fix: enable edge runtime on search

--- src/app/search/[query]/page.tsx
+++ src/app/search/[query]/page.tsx
@@ -1,5 +1,6 @@
 import Search from './views/Search'
 
+export const runtime = 'edge'
 export default function SearchPage({ params }: { params: { query: string } }) {
   return <Search query={params.query} />
 }
