d9d85a327b062fd1a760e7244f2104b5a09c5f3f
Liam Dyer <liamcdyer@gmail.com>
2024-01-23 14:38:53 -0500

    feat: reimplement relative time formatting

--- src/libs/format.ts
+++ src/libs/format.ts
@@ -1,27 +1,41 @@
-// import { formatDistance } from 'date-fns/formatDistance'
-// import { enGB } from 'date-fns/locale/en-GB'
-// import { formatRelative } from 'date-fns/formatRelative'
-
-export const formatDateAgo = (date: Date) => ''
-// formatDistance(date, new Date(), { addSuffix: true }).replace(/^about\s+/, '')
-
-// https://github.com/date-fns/date-fns/issues/1218#issuecomment-599182307
-const formatRelativeLocale = {
-	lastWeek: 'eeee',
-	yesterday: "'Yesterday'",
-	today: "'Today'",
-	tomorrow: "'Tomorrow'",
-	nextWeek: "'Next' eeee",
-	other: 'MMMM do, yyyy',
+export const formatDateAgo = (date: Date) => {
+	const currentDate = new Date()
+	const seconds = Math.floor((currentDate.getTime() - date.getTime()) / 1000)
+	const minutes = Math.floor(seconds / 60)
+	const hours = Math.floor(minutes / 60)
+	const days = Math.floor(hours / 24)
+	const weeks = Math.floor(days / 7)
+	const months = Math.floor(days / 30)
+	const years = Math.floor(days / 365)
+	if (years > 0) return `${years} year${years > 1 ? 's' : ''} ago`
+	if (months > 0) return `${months} month${months > 1 ? 's' : ''} ago`
+	if (weeks > 0) return `${weeks} week${weeks > 1 ? 's' : ''} ago`
+	if (days > 0) return `${days} day${days > 1 ? 's' : ''} ago`
+	if (hours > 0) return `${hours} hour${hours > 1 ? 's' : ''} ago`
+	if (minutes > 0) return `${minutes} minute${minutes > 1 ? 's' : ''} ago`
+	if (seconds > 0) return `${seconds} second${seconds > 1 ? 's' : ''} ago`
+	throw new Error('Invalid date')
 }
-/** Returns a date like "Today", "Yesterday", "Last Monday", "Next Friday", "January 1st, 2023" */
-export const formatDayRelative = (date: Date) => ''
-// formatRelative(date, new Date(), {
-// 	locale: {
-// 		...enGB,
-// 		formatRelative: (token: keyof typeof formatRelativeLocale) => formatRelativeLocale[token],
-// 	},
-// })
+
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+// todo: repeated code
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Nov', 'Dec']
+/** Returns a date like "Today", "Yesterday", "Last Monday", "January 1st, 2023" */
+export const formatDayRelative = (date: Date) => {
+	date.setHours(0, 0, 0, 0)
+	const currentDate = new Date()
+	currentDate.setHours(0, 0, 0, 0)
+
+	const daysAgo = Math.floor((currentDate.getTime() - date.getTime()) / 1000 / 60 / 60 / 24)
+	if (daysAgo < 0) {
+		throw new Error(`Date must be today or in the past but got: ${date.toISOString()}`)
+	}
+	if (daysAgo === 0) return 'Today'
+	if (daysAgo === 1) return 'Yesterday'
+	// todo: what if their week starts on the monday
+	if (daysAgo < 7) return `Last ${days[date.getDay()]}`
+	return `${months[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
+}
 
 export const formatNumberShort = (num: number) => {
 	if (num > 1_000_000_000) return `${(num / 1_000_000_000).toFixed(1)}B`
